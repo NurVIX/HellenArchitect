48348775ef58ab202ef9a2f8fe4ad6d50316010f
karinnarocha <karinna.rocha2@gmail.com>
2025-03-02 23:00:51 -0500

    Re-added frontend to the repository

---
+++ frontend/src/component/Pages/Interiores/interiores.js
@@ -0,0 +1,31 @@
+import React from "react";
+import "./interiores.css";
+import Hero from "../../Hero/hero";
+import ImagesGrid from "../../ImagesGrid/imagesGrid";
+
+
+
+
+const interiorGrid = [
+    {src: "/images/interior/interior_quartos.webp", title: "Quartos", className:"shortPhoto", link:"/quartos"},
+    {src: "/images/interior/interior_gourmet.webp", title: "Gourmet", className:"shortPhoto", link:"/gourmet"},
+    {src: "/images/interior/interior_lavabo.webp", title: "Lavabo", className:"shortPhoto", link:"/lavabo"},
+    {src: "/images/interior/interior_escritorio.webp", title: "Escritorio", className:"shortPhoto", link:"/escritorio"},
+    {src: "/images/interior/interior_sala.webp", title: "Sala", className:"shortPhoto", link:"/Sala"},
+    {src: "/images/interior/inteiror_outros.webp", title: "Outros", className:"shortPhoto", link:"/outros"},
+    {src: "/images/interior/interior_cozinha.webp", title: "Cozinha", className:"shortPhoto", link:"/cozinha"},
+    {src: "/images/mainpage/MainPage_Chafariz.webp", title: "Chafariz", className:"bottomImage"},
+    {src: "/images/interior_logo.webp", title: "Logo", className:"logoBottomPage"},
+];
+
+const Interior = () => {
+    console.log(interiorGrid)
+    return (
+        <>
+            <Hero image="../../images/interior/interior_HERO.webp"/>
+            <ImagesGrid images={interiorGrid}/>
+        </>
+    );
+};
+
+export default Interior;
---
+++ frontend/src/routes.js
@@ -0,0 +1,22 @@
+import React from "react";
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import Nav from "./component/Navbar/navbar";
+import Footer from "./component/Footer/footer"
+import Home from "./component/Pages/Home/home";
+import Interior from "./component/Pages/Interiores/interiores";
+
+
+const PagesRoutes = () => {
+    return (
+        <Router>
+            <Nav/>
+            <Routes>
+                <Route path="/" element={<Home/>}/>
+                <Route path="/interiores" element={<Interior/>}/>
+            </Routes>
+            <Footer/>
+        </Router>
+    );
+};
+
+export default PagesRoutes;
